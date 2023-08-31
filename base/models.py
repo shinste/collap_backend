25cfbef239b7240700009b1b0c157c07e1c50afc
tranjas <jasoncongtranjob@gmail.com>
2023-08-31 11:14:20 -0700

    Created Database templates for event and user logs

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
