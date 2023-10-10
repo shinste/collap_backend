20edbf00c226730bb1da1ec3a7605d7f2cdcdc84
Jason <tranjas1@uw.edu>
2023-10-09 17:11:35 -0700

    Added Serializers

--- base/serializers.py
+++ base/serializers.py
@@ -7,4 +7,36 @@ from .models import *
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('id','date', 'name', 'date_created', 'participants')
+        fields = '__all__'
+
+class EventUser(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class EventVote(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = '__all__'
+
+class EventUserEvent(serializers.ModelSerializer):
+    class Meta:
+        model = UserEvent
+        fields = '__all__'
+
+class EventNotifications(serializers.ModelSerializer):
+    class Meta:
+        model = EventNotifications
+        fields = '__all__'
+
+class EventAvailability(serializers.ModelSerializer):
+    class Meta:
+        model = Availability
+        fields = '__all__'
+
+class EventEventData(serializers.ModelSerializer):
+    class Meta:
+        model = EventDate
+        fields = '__all__'
+
+
