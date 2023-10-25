dce1e03dd4a67e2814df13a1e414d6b26e44b684
shinste <stephenshin1@hotmail.com>
2023-10-25 12:16:15 -0700

    revised create event endpoint and created vote endpoint

--- base/urls.py
+++ base/urls.py
@@ -14,7 +14,8 @@ urlpatterns = [
     path('ranked/', views.rank_dates, name="rank"),
     path('event/create/', CreateEvent.as_view(), name="create event"),
     path('event/view/', EventView.as_view(), name="event"),
-    path('notification/', views.notification_view , name="notifications"),
+    path('notification/', views.notification_view, name="notifications"),
     path('user/view_events/', views.ViewEvents, name='view_events'),
-    path('event/get_votes/', views.GetVotes, name="get_votes")
+    path('event/get_votes/', views.GetVotes, name="get_votes"),
+    path('vote/', Voting.as_view(), name="vote")
 ]
--- collap/settings.py
+++ collap/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    "django_rename_app",
 ]
 
 MIDDLEWARE = [
