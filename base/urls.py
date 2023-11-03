210459b87d2c823326b4d8afe8c3e8c8d85b8f2b
tranjas <jasoncongtranjob@gmail.com>
2023-11-02 18:18:01 -0700

    Removed view events (stephen did it)

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from django.contrib import admin
-from .views.viewEvents import *
 from .views.getVotes import *
 from .views.joinEvent import *
 from .views.pushVote import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/view_events/', viewEvents, name='view_events'),
     path('event/get_votes/', getVotes, name="get_votes"),
     path('event/join_event/', joinEvent.as_view(), name="join_event"),
     path('event/push_vote/', pushVote.as_view(), name ="push_vote")
