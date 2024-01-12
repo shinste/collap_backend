7a31d8d3e6995ae09796b30536860d8885e676ad
Stephen Shin <stephenshin@Stephens-Laptop.local>
2024-01-12 13:39:01 -0800

    exempt

--- base/views/register.py
+++ base/views/register.py
@@ -2,7 +2,9 @@ from rest_framework.generics import CreateAPIView
 from ..models import user
 from ..serializers import UserSerializer
 
+from django.views.decorators.csrf import csrf_exempt
 # Post Request that creates a new user
+@csrf_exempt
 class Register(CreateAPIView):
     queryset = user.objects.all()
     serializer_class = UserSerializer
