85b468b771354f09c7a8577bd16740f76e8d567e
shinste <stephenshin1@hotmail.com>
2024-01-14 18:39:45 -0800

    get token

--- base/views/getcsrftoken.py
+++ base/views/getcsrftoken.py
@@ -4,9 +4,11 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from django.middleware.csrf import get_token
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')
 class get_csrf_view(APIView):
     permission_classes = [AllowAny]
     def get(self, request, format=None):
+        get_token()
         return JsonResponse({'Success': 'CSRF Cookie set'})
