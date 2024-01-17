8f04ac772597974ec824fbcb1d6fe75cf1e4d734
shinste <stephenshin1@hotmail.com>
2024-01-17 14:00:40 -0800

    static

--- collap/deployment.py
+++ collap/deployment.py
@@ -21,6 +21,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
