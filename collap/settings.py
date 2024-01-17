e89eaa64c9ed716f4c69ae3fc27fc19c24fd0356
shinste <stephenshin1@hotmail.com>
2024-01-17 15:21:38 -0800

    secret

--- collap/settings.py
+++ collap/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') ## deploy setting
-SECRET_KEY = os.environ["SECRET"]
+SECRET_KEY = '13XdxKHyyK'
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
