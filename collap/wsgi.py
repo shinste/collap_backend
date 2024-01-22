9b1af205b7464cfbbfd57164825f5bc54eaed05c
shinste <stephenshin1@hotmail.com>
2024-01-22 12:43:59 -0800

    wsgi not detecting production

--- collap/wsgi.py
+++ collap/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = 'collap.production' if 'WEBSITE_HOSTNAME' in os.environ else 'collap.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collap.production')
 
 application = get_wsgi_application()
 
