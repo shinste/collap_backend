3c209ec29bae50a54a25f3f22d63febe8bfcf790
shinste <stephenshin1@hotmail.com>
2024-01-22 11:45:15 -0800

    tutorial

--- collap/wsgi.py
+++ collap/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'collap.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'collap.settings'
+settings_module = 'collap.production' if 'WEBSITE_HOSTNAME' in os.environ else 'collap.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
