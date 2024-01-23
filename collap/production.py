575615ed6b05b5c383e47dfd2cd9799937c4d7dd
shinste <stephenshin1@hotmail.com>
2024-01-22 17:10:50 -0800

    credentials

--- collap/production.py
+++ collap/production.py
@@ -29,10 +29,10 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': conn_str_params['DBNAME'],
+        'HOST': conn_str_params['DBHOST'],
+        'USER': conn_str_params['DBUSER'],
+        'PASSWORD': conn_str_params['DBPASSWORD'],
         'PORT': 5432
     }
 }
