d1546c17954341cd6cd1509826fb5aa98691d730
shinste <stephenshin1@hotmail.com>
2024-01-24 13:04:56 -0800

    why is it not updating?

--- collap/production.py
+++ collap/production.py
@@ -40,11 +40,10 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['name'],
+        'NAME': conn_str_params['dbname'],
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
         'PASSWORD': conn_str_params['password'],
-        'PORT': 5432
     }
 }
 
