44e8a24a047de068218b760699d9796693de42aa
shinste <stephenshin1@hotmail.com>
2024-01-26 15:14:09 -0800

    primary bug

--- base/views/changeprimary.py
+++ base/views/changeprimary.py
@@ -7,22 +7,25 @@ from ..serializers import NotificationSerializer, EventDateSerializer
 class ChangePrimary(CreateAPIView):
     def create(self, request, *args, **kwargs):
         # Extracting data from request and checking missing information
-        entire_data = request.data
-        start = entire_data.get("start")
-        end = entire_data.get("end")
-        event_id = entire_data.get("event_id")
-        primary = entire_data.get("primary")
-        primary_end = entire_data.get('primary_end')
-        if not start or not end or not event_id or not primary or not primary_end:
-            return JsonResponse({'error':'Missing Input'}, status=400)
+        try:
+            entire_data = request.data
+            start = entire_data.get("start")
+            end = entire_data.get("end")
+            event_id = entire_data.get("event_id")
+            primary = entire_data.get("primary")
+            primary_end = entire_data.get('primary_end')
+            if not start or not end or not event_id or not primary or not primary_end:
+                return JsonResponse({'error':'Missing Input'}, status=400)
 
-        changes = Event.objects.get(event_id=event_id)
-        if changes.primary_date == primary:
-            return JsonResponse({'error': 'Date already the Primary Date'}, status=400)
-        else:
-            changes.primary_date = primary
-            changes.primary_end = primary_end
-            changes.start = start
-            changes.end = end
-            changes.save()
-        return JsonResponse({"status":"success"}, status=200)
+            changes = Event.objects.get(event_id=event_id)
+            if changes.primary_date == primary:
+                return JsonResponse({'error': 'Date already the Primary Date'}, status=400)
+            else:
+                changes.primary_date = primary
+                changes.primary_end = primary_end
+                changes.start = start
+                changes.end = end
+                changes.save()
+            return JsonResponse({"status":"success"}, status=200)
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
