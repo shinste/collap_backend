6005fd469a9be7f2064dad072e0f38446f1f9855
shinste <stephenshin1@hotmail.com>
2024-01-30 13:05:35 -0800

    login get -> post

--- base/views/login.py
+++ base/views/login.py
@@ -4,9 +4,11 @@ from ..models import user
 
 # Get Request that checks the username's password in database (temporary?)
 class Login(ListAPIView):
-    def list(self, request, *args, **kwargs):
-        username = request.GET.get('username')
-        password = request.GET.get('password')
+    def create(self, request, *args, **kwargs):
+        entire_data = request.data
+        username = entire_data.get('username')
+        password = entire_data.get('password')
+
         if not username or not password:
             return JsonResponse({'error':'Missing Input'}, status=400)
         try:
