0bd979b3598dd1c4d1f4401310fb08d4e28441e6
shinste <stephenshin1@hotmail.com>
2024-02-26 10:59:03 -0800

    exception

--- startup.sh
+++ startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+
+# Redirect stdout and stderr to a log file
+exec > >(tee -i /var/log/myapp.log)
+exec 2>&1
+
+# Run collectstatic
+python manage.py collectstatic --noinput
+
+# Run Gunicorn with your Django application
+gunicorn --workers 2 myproject.wsgi
