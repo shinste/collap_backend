eec87e9c0779cc929050ec139544f676e2e16508
shinste <stephenshin1@hotmail.com>
2024-02-26 18:59:09 -0800

    echo

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+echo "1"
 # Redirect stdout and stderr to a log file
 exec > >(tee -i /var/log/myapp.log)
 exec 2>&1
-
+echo "2"
 # Run collectstatic
 python manage.py collectstatic --noinput
 
+echo "3"
 # Run Gunicorn with your Django application
 gunicorn --workers 2 collap.wsgi
