aabb3931400522d84da06d534c7df7fe874b4917
shinste <stephenshin1@hotmail.com>
2024-02-28 14:01:25 -0800

    plain run

--- collap/wsgi.py
+++ collap/wsgi.py
@@ -19,10 +19,9 @@ sys.path.append('/mnt/d/Work/Software_Projects/collap_backend')
 settings_module = 'collap.settings' 
 # if 'WEBSITE_HOSTNAME' in os.environ else 'collap.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-from whitenoise import WhiteNoise
-application = WhiteNoise(get_wsgi_application())
+# from whitenoise import WhiteNoise
+application = get_wsgi_application()
 
-serve(application, port=8000)
 
 
 # settings_module = 'collap.production' if 'WEBSITE_HOSTNAME' in os.environ else 'collap.settings'
