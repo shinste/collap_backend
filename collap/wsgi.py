b80d61440b2692497500aedf2cd6ce24ace00a1a
shinste <stephenshin1@hotmail.com>
2024-02-28 13:56:49 -0800

    stupid file thing

--- collap/wsgi.py
+++ collap/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 import sys
 import os
+from waitress import serve
 
 from django.core.wsgi import get_wsgi_application
 
@@ -20,6 +21,9 @@ settings_module = 'collap.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 from whitenoise import WhiteNoise
 application = WhiteNoise(get_wsgi_application())
+
+serve(application, port=8000)
+
 
 # settings_module = 'collap.production' if 'WEBSITE_HOSTNAME' in os.environ else 'collap.settings'
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collap.production')
