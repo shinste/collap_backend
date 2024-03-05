0dffbe488a4c1ad9eda8ff0212a5882e93239024
shinste <stephenshin1@hotmail.com>
2024-03-05 11:54:01 -0800

    db issues

--- base/views/deletenotification.py
+++ base/views/deletenotification.py
@@ -6,15 +6,18 @@ from ..models import Notification, Event
 class DeleteNotifications(CreateAPIView):
     def create(self, request, *args, **kwargs):
         # Extracting data from request and checking missing information
-        entire_data = request.data
-        username = entire_data.get("username")
-        event_id = entire_data.get("event_id")
-        notification = entire_data.get("notification")
-        if not username or not event_id or not notification:
-            return JsonResponse({'error':'Missing Input'}, status=400)
-        notification_instance = Notification.objects.filter(event_id=event_id, username=username, notification=notification)
-        if not notification_instance:
-            return JsonResponse({'error': "No notification found for this event/participant."}, status=400)
-        notification_instance.delete()
-        return JsonResponse({"status":"success"}, status=200)
+        try:
+            entire_data = request.data
+            username = entire_data.get("username")
+            event_id = entire_data.get("event_id")
+            notification = entire_data.get("notification")
+            if not username or not event_id or not notification:
+                return JsonResponse({'error':'Missing Input'}, status=400)
+            notification_instance = Notification.objects.filter(event_id=event_id, username=username, notification=notification)
+            if not notification_instance:
+                return JsonResponse({'error': "No notification found for this event/participant."}, status=400)
+            notification_instance.delete()
+            return JsonResponse({"status":"success"}, status=200)
+        except Exception as error:
+            return JsonResponse({'error': f'{error}'})
 
